ea288a0d2b40156a652c402f1b5df3118dd032d0
xianhuawei <xianhuawei.zhcn@qq.com>
2018-01-01 23:02:11 +0800

    icdiff

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 yum install -y epel-release
 yum install tig jq htop ShellCheck tmux the_silver_searcher python34-setuptools
-pip install tldr mycli yapf youtube-dl
-easy_install-3.4 you-get
+pip install tldr mycli yapf youtube-dl icdiff
+easy_install-3.6 you-get
